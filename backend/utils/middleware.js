b3d3840e4895272076ea3a0e13caab89a97710ea
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-03-10 20:44:32 +0530

    Update middleware.js

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,7 +1,7 @@
 const logger = require("./logger");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
-const Librarian = require("../models/librarian");
+// const User = require("../models/user");
+// const Librarian = require("../models/librarian");
 
 const requestLogger = (request, response, next) => {
   logger.info("Method:", request.method);
@@ -18,11 +18,12 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message);
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  } else if (error.name === "JsonWebTokenError") {
+  // if (error.name === "CastError") {
+  //   return response.status(400).send({ error: "malformatted id" });
+  // } else if (error.name === "ValidationError") {
+  //   return response.status(400).json({ error: error.message });
+  // } else 
+  if (error.name === "JsonWebTokenError") {
     return response.status(401).json({ error: error.message });
   } else if (error.name === "TokenExpiredError") {
     return response.status(401).json({ error: "token expired" });
@@ -42,41 +43,41 @@ const tokenExtractor = (request, response, next) => {
   next();
 };
 
-const userExtractor = async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
-  if (user) {
-    request.user = user;
-  } else {
-    request.user = null;
-  }
+// const userExtractor = async (request, response, next) => {
+//   const decodedToken = jwt.verify(request.token, process.env.SECRET);
+//   if (!decodedToken.id) {
+//     return response.status(401).json({ error: "token invalid" });
+//   }
+//   const user = await User.findById(decodedToken.id);
+//   if (user) {
+//     request.user = user;
+//   } else {
+//     request.user = null;
+//   }
 
-  next();
-};
+//   next();
+// };
 
-const librarianExtractor = async (request, response, next) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const librarian = await Librarian.findById(decodedToken.id);
-  if (librarian) {
-    request.librarian = librarian;
-  } else {
-    request.librarian = null;
-  }
+// const librarianExtractor = async (request, response, next) => {
+//   const decodedToken = jwt.verify(request.token, process.env.SECRET);
+//   if (!decodedToken.id) {
+//     return response.status(401).json({ error: "token invalid" });
+//   }
+//   const librarian = await Librarian.findById(decodedToken.id);
+//   if (librarian) {
+//     request.librarian = librarian;
+//   } else {
+//     request.librarian = null;
+//   }
 
-  next();
-};
+//   next();
+// };
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
   errorHandler,
   tokenExtractor,
-  userExtractor,
-  librarianExtractor,
+  // userExtractor,
+  // librarianExtractor,
 };
