12fe86c78f7d2bd1ae389256e3e0afd4def93b92
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-03-14 17:38:39 +0530

    Update programme.sql

--- schemas/programme.sql
+++ schemas/programme.sql
@@ -1,6 +1,7 @@
 CREATE TABLE programme (
     id INT PRIMARY KEY AUTO_INCREMENT,
     dept_name VARCHAR(50) NOT NULL,
+    degree VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
     FOREIGN KEY (dept_name) REFERENCES department(name) ON DELETE CASCADE
 );
