5381e714fa733b8da3249db69036a0de3e69ce49
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-03-15 09:07:12 +0530

    Update student.sql

--- schemas/student.sql
+++ schemas/student.sql
@@ -3,5 +3,6 @@ CREATE TABLE student (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     programme_id INT NOT NULL,
+    year_of_joining INT NOT NULL,
     FOREIGN KEY (programme_id) REFERENCES programme(id) ON DELETE CASCADE
 );
