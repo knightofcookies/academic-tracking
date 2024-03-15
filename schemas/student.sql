9a325bee1f2a6385771997e3d2bdf6768c63bd31
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-03-15 09:18:25 +0530

    Add year_of_joining to student

--- schemas/student.sql
+++ schemas/student.sql
@@ -1,7 +1,8 @@
 CREATE TABLE student (
     roll INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     programme_id INT NOT NULL,
+    year_of_joining YEAR NOT NULL,
     FOREIGN KEY (programme_id) REFERENCES programme(id) ON DELETE CASCADE
 );
