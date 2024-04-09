ad0797c53d813ca7a6f46be290f4cba92bcdb799
schrodingerszanycat <121005817+schrodingerszanycat@users.noreply.github.com>
2024-04-09 16:09:11 +0530

    Updated AddStudentPage.jsx

--- frontend/src/components/AddStudentPage.jsx
+++ frontend/src/components/AddStudentPage.jsx
@@ -1,3 +1,140 @@
+// import React, { useState, useEffect } from 'react';
+// import { Typography, TextField, Button, Paper, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+// import { CssBaseline } from '@mui/material';
+// import ResponsiveAppBar from './ResponsiveAppBar';
+// import ErrorMessage from './ErrorMessage';
+// import SideBar from './SideBar';
+// import { Box } from '@mui/system';
+// import adminServices from '../services/admin';
+
+// const AddStudentPage = () => {
+//   const [errorMessage, setErrorMessage] = useState("");
+//   const [programmes, setProgrammes] = useState([]); // State to store department names
+//   const [selectedProgramme, setSelectedProgramme] = useState(""); // State to store selected department name
+//   const user = JSON.parse(localStorage.getItem("loggedAcademicTrackingAdmin"));
+
+//   useEffect(() => {
+//     // Fetch department names when component mounts
+//     adminServices.setToken(user.token)
+//     adminServices.getAllProgrammes()
+//       .then(data => {
+//         setProgrammes(data);
+//         console.log(data);
+//       })
+//       .catch(error => console.error("Error fetching programmes:", error));
+//   }, []);
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const formData = new FormData(event.currentTarget);
+//     const roll = formData.get('roll');
+//     const name = formData.get('name');
+//     const email = formData.get('email');
+//     const year_of_joining = formData.get('year_of_joining');    
+//     if (!roll || 
+//         !name || 
+//         !email || 
+//         !selectedProgramme 
+//         || !year_of_joining) {
+//       setErrorMessage("Missing required fields");
+//       setTimeout(() => {
+//         setErrorMessage("");
+//       }, 5000);
+//       return;
+//     }
+    
+//     const programmeId = programmes.find(prog => prog.name === selectedProgramme)?.id;
+//     if (!programmeId) {
+//       setErrorMessage("Invalid programme selected");
+//       setTimeout(() => {
+//         setErrorMessage("");
+//       }, 5000);
+//       return;
+//     }
+
+//     const credentials = {
+//       roll: roll,
+//       name: name,
+//       email: email,
+//       programme_id: programmeId,
+//       year_of_joining: year_of_joining
+//     };
+
+//     adminServices.setToken(user?.token);
+//     adminServices
+//       .addStudent(credentials)
+//       .then(() => {
+//         alert("Student Added Successfully!!!");
+//         event.target.reset();
+//       })
+//       .catch((error) => {
+//         if (error.response.data.error) {
+//           setErrorMessage(error.response.data.error);
+//           setTimeout(() => {
+//             setErrorMessage("");
+//           }, 5000);
+//         } else {
+//           setErrorMessage(
+//             "Error adding student. Please check the console for more details"
+//           );
+//           console.error("Error adding student:", error);
+//           setTimeout(() => {
+//             setErrorMessage("");
+//           }, 5000);
+//         }
+//       });
+//   }
+
+//   return (
+//     <div>
+//       <ErrorMessage errorMessage={errorMessage} />
+//       <Box display="flex">
+//         <SideBar />
+//         <Box flexGrow={1}>
+//           <ResponsiveAppBar />
+//           <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundImage: 'linear-gradient(to right, #667db6, #0082c8, #0082c8, #667db6)' }}>
+//             <CssBaseline />
+//             <Paper style={{ padding: 24, borderRadius: 8 }}>
+//               <Typography variant="h5" align="center" gutterBottom>
+//                 Add Student
+//               </Typography>
+//               <form onSubmit={handleSubmit}>
+//                 <div style={{ width: '100%' }}>
+
+//                   <TextField label="Roll number" variant="outlined" fullWidth margin="normal" id='roll' name='roll' />
+//                   <TextField label="Name" variant="outlined" fullWidth margin="normal" id='name' name='name' />
+//                   <TextField label="Email ID" variant="outlined" fullWidth margin="normal" id='email' name='email' />
+
+//                   <FormControl fullWidth margin="normal">
+//                     <InputLabel id="programme-label">Programme</InputLabel>
+//                     <Select
+//                       labelId="programme-label"
+//                       id="programme"
+//                       value={selectedProgramme}
+//                       onChange={(event) => setSelectedProgramme(event.target.value)}
+//                     >
+//                       {programmes.map((prog) => (
+//                         <MenuItem key={prog.id} value={prog.name}>{prog.name}</MenuItem>
+//                       ))}
+//                     </Select>
+//                   </FormControl>
+
+//                   <TextField label="Year of Joining" variant="outlined" fullWidth margin="normal" id='year_of_joining' name='year_of_joining' />
+//                   <Button variant="contained" color="primary" style={{ marginTop: 24 }} fullWidth type='submit'>
+//                     Add Student
+//                   </Button>
+//                 </div>
+//               </form>
+//             </Paper>
+//           </div>
+//         </Box>
+//       </Box>
+//     </div>
+//   );
+// };
+
+// export default AddStudentPage;
+
 import React, { useState, useEffect } from 'react';
 import { Typography, TextField, Button, Paper, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import { CssBaseline } from '@mui/material';
@@ -11,6 +148,7 @@ const AddStudentPage = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [programmes, setProgrammes] = useState([]); // State to store department names
   const [selectedProgramme, setSelectedProgramme] = useState(""); // State to store selected department name
+  const [selectedDegree, setSelectedDegree] = useState(""); // State to store selected department name
   const user = JSON.parse(localStorage.getItem("loggedAcademicTrackingAdmin"));
 
   useEffect(() => {
@@ -42,9 +180,11 @@ const AddStudentPage = () => {
       }, 5000);
       return;
     }
-    const programmeId = programmes.find(prog => prog.name === selectedProgramme)?.id;
+    
+    const programmeId = programmes.find(prog => (prog.degree == selectedDegree && 
+                                        prog.name === selectedProgramme))?.id;
     if (!programmeId) {
-      setErrorMessage("Invalid programme selected");
+      setErrorMessage("Invalid combination of degree and name selected!");
       setTimeout(() => {
         setErrorMessage("");
       }, 5000);
@@ -84,6 +224,9 @@ const AddStudentPage = () => {
       });
   }
 
+  const uniqueDegrees = [...new Set(programmes.map(prog => prog.degree))];
+  const uniqueProgrammeNames = [...new Set(programmes.map(prog => prog.name))];
+
   return (
     <div>
       <ErrorMessage errorMessage={errorMessage} />
@@ -105,6 +248,20 @@ const AddStudentPage = () => {
                   <TextField label="Email ID" variant="outlined" fullWidth margin="normal" id='email' name='email' />
 
                   <FormControl fullWidth margin="normal">
+                    <InputLabel id="degree-label">Degree</InputLabel>
+                    <Select
+                      labelId="degree-label"
+                      id="degree"
+                      value={selectedDegree}
+                      onChange={(event) => setSelectedDegree(event.target.value)}
+                    >
+                      {uniqueDegrees.map(degree => (
+                        <MenuItem key={degree} value={degree}>{degree}</MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+
+                  <FormControl fullWidth margin="normal">
                     <InputLabel id="programme-label">Programme</InputLabel>
                     <Select
                       labelId="programme-label"
@@ -112,8 +269,8 @@ const AddStudentPage = () => {
                       value={selectedProgramme}
                       onChange={(event) => setSelectedProgramme(event.target.value)}
                     >
-                      {programmes.map((prog) => (
-                        <MenuItem key={prog.id} value={prog.name}>{prog.name}</MenuItem>
+                      {uniqueProgrammeNames.map(name => (
+                        <MenuItem key={name} value={name}>{name}</MenuItem>
                       ))}
                     </Select>
                   </FormControl>
