7e0f71cb033b072c23d8ae60580eadd14029ef88
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-04-17 09:43:44 +0530

    "Update ProgrammePage to handle errors when fetching programmes and add SideBar"
    This commit updates the ProgrammePage component to handle errors when fetching programmes by setting an error message that is displayed for 5 seconds. Additionally

--- frontend/src/components/ProgrammePage.jsx
+++ frontend/src/components/ProgrammePage.jsx
@@ -1,49 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { CssBaseline } from '@mui/material';
-import ResponsiveAppBar from './ResponsiveAppBar';
+import { useEffect, useState } from 'react'
 import ErrorMessage from './ErrorMessage';
 import adminServices from '../services/admin';
 import CardAnalytics from './CardAnalytics';
 import Grid from '@mui/material/Grid';
 import Programme from '../assets/programme_image.png';
-import '../styles/ProgrammePage.css'; 
+import '../styles/ProgrammePage.css';
+import SideBar from './SideBar';
 
 const ProgrammePage = () => {
   const [programmes, setProgrammes] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const user = JSON.parse(localStorage.getItem("loggedAcademicTrackingAdmin") || localStorage.getItem("loggedAcademicTrackingUser"));
-  
+
   useEffect(() => {
     adminServices.setToken(user?.token);
     adminServices
-            .getAllProgrammes()
-            .then((data) => {
-                setProgrammes(data);
-                console.log(data);
-            })
-            .catch((error) =>
-                console.error("Error fetching Programmes:", error)
-            );
-    
-  }, [])
+      .getAllProgrammes()
+      .then((data) => {
+        setProgrammes(data);
+      })
+      .catch((error) => {
+        setErrorMessage("Error fetching programmes.");
+        console.error(error);
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 5000);
+      });
+  }, [user?.token]);
 
   return (
-    <div>
-      <CssBaseline />
-      <ResponsiveAppBar />
+    <SideBar>
       <ErrorMessage errorMessage={errorMessage} />
       <div className="container-programme">
         <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-              {programmes.map(prog => (
-              <Grid item xs={2} sm={4} md={4} key={prog.id}>
-                <CardAnalytics title = {prog.degree}
-                              subTitle = {prog.name} 
-                              image_src = {Programme}/>
-                </Grid>
-                ))}
+          {programmes.map(prog => (
+            <Grid item xs={2} sm={4} md={4} key={prog.id}>
+              <CardAnalytics title={prog.degree}
+                subTitle={prog.name}
+                image_src={Programme} />
+            </Grid>
+          ))}
         </Grid>
       </div>
-    </div>
+    </SideBar>
   )
 };
 
